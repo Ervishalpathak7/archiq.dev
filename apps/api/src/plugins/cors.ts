4e440e0a166e1f462e56c5e3b5b1ae874f8e9b26
Vishal Pathak <vishalpathak.er@gmail.com>
2026-04-01 13:38:30 +0530

    feat: implement dynamic CORS origin handling based on frontend URL

--- apps/api/src/plugins/cors.ts
+++ apps/api/src/plugins/cors.ts
@@ -1,9 +1,16 @@
 import fp from "fastify-plugin";
 import cors from "@fastify/cors";
+import { config } from "../config/index.js";
 
 export const corsPlugin = fp(async (fastify) => {
   fastify.register(cors, {
-    origin: "*",
+    origin: (origin, callback) => {
+      if (config.FRONTEND_URL && origin === config.FRONTEND_URL) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"), false);
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     exposedHeaders: ["Content-Length"],
