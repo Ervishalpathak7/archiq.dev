8dbbbc524c8fff7b1faf44769f947bf32a6544f8
Vishal Pathak <vishalpathak.er@gmail.com>
2026-04-04 10:07:43 +0530

    chore: remove all current files and restarting the project

--- apps/api/src/plugins/jwt.ts
+++
@@ -1,7 +0,0 @@
-import fp from "fastify-plugin";
-import jwt from "@fastify/jwt";
-import { config } from "../config/index.js";
-
-export const jwtPlugin = fp(async (fastify) => {
-  fastify.register(jwt, { secret: config.JWT_ACCESS_SECRET });
-});
